4a0326de2d8d756420b0a073c01f715e552222b7
Nix3732 <116956559+Nix3732@users.noreply.github.com>
2024-03-20 14:10:24 +0200

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,8 +1,11 @@
 city_main = set()
 n = int(input())
 i = 0
-letter = ''
-while i < n:
+city_local = input()
+letter = city_local[-1]
+city_main.add(city_local)
+print("OK")
+while i < n-1:
     city_local = input()
     if (city_local in city_main) or (city_local[0] != letter):
         print("REPEAT")
